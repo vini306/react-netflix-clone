0d8bb722b578291cd1e5730052abeb68f3cfad78
vinicius dos santos mota <vinicius_dos_santos_mota@hotmail.com>
2020-10-16 03:43:41 -0300

    - Add jumbotron component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import jumboData from  './fixtures/jumbo.json';
+import Jumbrotron from './components/jumbotron';
 export default function App() {
   return (
     <div className="App">
-     <h1> Hello World</h1>
+      <Jumbrotron.Container>
+        {jumboData.map((item) => (
+          <Jumbrotron key={item.id} direction={item.direction}>
+            <Jumbrotron.Pane>
+              <Jumbrotron.Title>{item.title}</Jumbrotron.Title>
+              <Jumbrotron.SubTitle>{item.subTitle}</Jumbrotron.SubTitle>
+            </Jumbrotron.Pane>
+            <Jumbrotron.Pane>
+              <Jumbrotron.Image src={item.image} alt={item.alt} />            
+            </Jumbrotron.Pane>
+          </Jumbrotron>
+        ))}
+      </Jumbrotron.Container>
     </div>
   );
 }
